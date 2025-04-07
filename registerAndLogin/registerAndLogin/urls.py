50d9c7be90c3a01139b695ca54b22b06fc7ad40d
KJH198 <kjh15071687155@126.com>
2025-04-07 15:27:56 +0800

    fix

--- registerAndLogin/registerAndLogin/urls.py
+++ registerAndLogin/registerAndLogin/urls.py
@@ -27,6 +27,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('user/', include(('user.urls', 'user'), namespace='user')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
