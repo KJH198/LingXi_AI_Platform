f3a96f6bb75cfc2eb4b1f4d42a7c8b91ae3cc419
周运贵 <22371276@buaa.edu.cn>
2025-05-04 22:26:25 +0800

    搜索

--- registerAndLogin/community/urls.py
+++ registerAndLogin/community/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 现有路由
     path('posts/', views.get_post_list, name='get_post_list'),
-] 
+    
+    # 添加搜索路由
+    path('search', views.SearchView.as_view(), name='search'),
+]
